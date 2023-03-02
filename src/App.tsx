06465ad5e401b0a1337dba21324b852b48d47cc8
hasantunahan <hasan.ak@ekinokssoftware.com>
2023-03-02 20:14:48 +0300

    feat: env add

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from 'react';
 import './App.css';
 import UserDetails from './page/UserDetails';
 
 function App() {
+
+  useEffect(() => {
+    console.log('====================================');
+    console.log(`Processs ${process.env.REACT_APP_ANY_OTHER_VAR}`);
+    console.log(`Processs ${JSON.stringify(process.env)}`);
+    console.log('====================================');
+  }, [])
+
+
   return (
     <div className="App">
       <div>
