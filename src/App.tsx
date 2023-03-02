07592e28f78333c1c70527b4de1be7f32634587f
hasantunahan <hasan.ak@ekinokssoftware.com>
2023-03-02 22:47:26 +0300

    feat: webstorm configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import { useEffect } from 'react';
+import {useEffect} from 'react';
 import './App.css';
 import UserDetails from './page/_features/user_details/UserDetails';
 
 function App() {
 
-  useEffect(() => {
-    console.log('====================================');
-    console.log(`Processs ${process.env.REACT_APP_ANY_OTHER_VAR}`);
-    console.log(`Processs ${JSON.stringify(process.env)}`);
-    console.log('====================================');
-  }, [])
+    useEffect(() => {
+        console.log('====================================');
+        console.log(`Processs ${process.env.REACT_APP_ANY_OTHER_VAR}`);
+        console.log(`Processs ${JSON.stringify(process.env)}`);
+        console.log('====================================');
+    }, [])
 
 
-  return (
-    <div className="App">
-      <div>
-        hasantunahan
-      </div>
-      <UserDetails id="2" />
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>
+                hasantunahan
+            </div>
+            <UserDetails id="2"/>
+        </div>
+    );
 }
 
 export default App;
