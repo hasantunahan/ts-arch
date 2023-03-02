b937e99eb2595ff9c6d144d1d35ecb6a3c6a6375
hasantunahan <hasan.ak@ekinokssoftware.com>
2023-03-02 22:51:15 +0300

    feat: cleanup

--- src/page/_features/user_details/UserDetails.tsx
+++ src/page/_features/user_details/UserDetails.tsx
@@ -1,35 +1,34 @@
-import { useCallback, useEffect } from "react";
 import './UserDetails.css'
 import useUser from "../../../hooks/user/useUser";
 
 function UserDetails(props: { id: string }) {
-  const { user, loading } = useUser("2");
+    const {user, loading} = useUser("2");
 
 
-  return _renderState();
+    return _renderState();
 
-  function _renderState() {
-    if (loading) {
-      return <div>Loading app..</div>
-    } else {
-      return _renderUserDetail()
+    function _renderState() {
+        if (loading) {
+            return <div>Loading app..</div>
+        } else {
+            return _renderUserDetail()
+        }
     }
-  }
 
-  function _renderUserDetail() {
-    return <div className="infomation_user">
-      <div className="combine_image">
-        <div className="user_name">
-          {user.id} {user.pantone_value}
-        </div>
+    function _renderUserDetail() {
+        return <div className="infomation_user">
+            <div className="combine_image">
+                <div className="user_name">
+                    {user.id} {user.pantone_value}
+                </div>
 
-      </div>
-      <div className="user_fullname">
-        <div className="user_title">
+            </div>
+            <div className="user_fullname">
+                <div className="user_title">
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  }
+    }
 }
 
 export default UserDetails;
